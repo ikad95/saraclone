8f8271157ac779d40391f086b0fff909c23bc93e
Moulik Adak <moulik.adak@gmail.com>
2017-08-11 14:35:00 +0530

    Hi

---
+++ resources/views/index.blade.php
@@ -0,0 +1,134 @@
+ 
+<!DOCTYPE html>
+<html dir="rtl" class="Home Index">
+<head prefix="og: http://ogp.me/ns#">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
+    <meta name="theme-color" content="#10bbb3">
+    <meta property="og:image" content="https://www.sarahah.com/img/Sarahah-rich.png" />
+    <meta property="og:description" content=Sarahah helps you in receiving constructive honest feedback while maintaining privacy />
+    <meta name="verify-admitad" content="c45775418a" />
+    <title>Darkie </title>
+    <link rel="apple-touch-icon" href="img/icon.png">
+    <link href="https://fonts.googleapis.com/css?family=Cairo:400,700&amp;subset=arabic,latin-ext" rel="stylesheet">
+    <link href="css/toolkit-rtl.min.css" rel="stylesheet" />
+    <link href="css/application-rtl.min.css" rel="stylesheet">
+            <link href="css/toolkit-ltr.min.css" rel="stylesheet">
+    <link rel="icon" src="~/img/favicon.png">
+    <script src="lib/jquery/dist/jquery.min.js"></script>
+    <script type="text/javascript">
+        var appInsights=window.appInsights||function(config){
+            function i(config){t[config]=function(){var i=arguments;t.queue.push(function(){t[config].apply(t,i)})}}var t={config:config},u=document,e=window,o="script",s="AuthenticatedUserContext",h="start",c="stop",l="Track",a=l+"Event",v=l+"Page",y=u.createElement(o),r,f;y.src=config.url||"https://az416426.vo.msecnd.net/scripts/a/ai.0.js";u.getElementsByTagName(o)[0].parentNode.appendChild(y);try{t.cookie=u.cookie}catch(p){}for(t.queue=[],t.version="1.0",r=["Event","Exception","Metric","PageView","Trace","Dependency"];r.length;)i("track"+r.pop());return i("set"+s),i("clear"+s),i(h+a),i(c+a),i(h+v),i(c+v),i("flush"),config.disableExceptionTracking||(r="onerror",i("_"+r),f=e[r],e[r]=function(config,i,u,e,o){var s=f&&f(config,i,u,e,o);return s!==!0&&t["_"+r](config,i,u,e,o),s}),t
+        }({
+            instrumentationKey: "1f1333b5-6967-4f49-afe7-1b620c015917",
+            disableExceptionTracking: true,
+            disableAjaxTracking: true,
+            enableDebug: false,
+            
+            
+        });
+       
+        window.appInsights=appInsights;
+        appInsights.trackPageView();
+    </script>
+    
+</head>
+<body class="with-top-navbar">
+    <div class="growl" id="app-growl"></div>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top app-navbar">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-main">
+                    <span class="sr-only">Menu</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a href="https://" class="navbar-brand">
+                    <span><img style="padding-bottom:7px;" src="img/Logo.png" /><span class="hidden-sm hidden-xs hidden-xxs">Clone :P</span></span>
+                    <!--Remove the x in "xclass in the img above when the search bar is shown to removed "sarahah" and keep the logo only on mobile screens-->
+                </a>
+                <div class="col-xs-3">
+                </div>
+            </div>
+            <div class="navbar-collapse collapse" id="navbar-collapse-main">
+                <ul class="nav navbar-nav navbar-right">
+                    
+    <li><a href="Home/SetLanguage?culture=en&amp;returnUrl=%252FAccount%252FRegister">Register</a></li>
+    <li><a href="Home/SetLanguage?culture=en&amp;returnUrl=%252FAccount%252FLogin">Login</a></li>
+
+                    <li>
+                        <a href="Home/SetLanguage?culture=en&amp;returnUrl=%252FHome%252FAbout">About Us</a>
+                    </li>
+                    <li>
+                        <a href="Home/SetLanguage?culture=en&amp;returnUrl=%252FHome%252FContact">Contact Us</a>
+                    </li>
+                </ul>
+
+            </div>
+        </div>
+    </nav>
+    <div class="container container-mobile p-t-md">
+
+        
+
+<div class="row">
+    <div class="col-md-12">
+        Get honest feedback from your coworkers and friends<br /><br />
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-6">
+        <h4>At work</h4>
+        <ul>
+            <li>Enhance your areas of strength</li>
+            <li>Strengthen Areas for Improvement</li>
+        </ul>
+    </div>
+
+    <div class="col-md-6">
+        <h4>With Your Friends</h4>
+        <ul>
+            <li>Improve your friendship by discovering your strengths and areas for improvement</li>
+            <li>Let your friends be honest with you</li>
+        </ul>
+    </div>
+</div>
+
+<br />
+<div class="row">
+    <div class="col-md-12">
+        &nbsp;
+<a style="font-weight:bold" href="Home/SetLanguage?culture=en&amp;returnUrl=%252FAccount%252FRegister">Register</a> | <a style="font-weight: bold" href="Home/SetLanguage?culture=en&amp;returnUrl=%252FAccount%252FLogin">Login</a>    </div>
+</div>
+
+
+
+    </div>
+    <footer class="center-block">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-5 col-md-4 col-lg-4">
+                    <div class="copyright">
+                        Darkie 2017 &copy;-
+                        <a href="Home/SetLanguage?culture=en&amp;returnUrl=%252FHome%252FPrivacy">Privacy</a>  -
+                        <a href="Home/SetLanguage?culture=en&amp;returnUrl=%252FHome%252FTerms">Terms</a>
+
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-2 col-md-4 col-lg-4">
+                    <div>
+                        <a class="spacer" href="Home/SetLanguage?culture=en&amp;returnUrl=%252F">English</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <script src="js/toolkit.min.js"></script>
+    
+    
+
+</body>
+</html>
